60b00661d0fa9fdc9d5d0d2c9e661228fdeb70d8
RosemondeT <traore.rose.benedicte@gmail.com>
2020-03-05 10:51:49 +0100

    modification de la classe de test

--- src/test/java/traore/Exercice_OCP/EmployeTest.java
+++ src/test/java/traore/Exercice_OCP/EmployeTest.java
@@ -1,5 +1,7 @@
 package traore.Exercice_OCP;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 
 
@@ -14,6 +16,7 @@ public class EmployeTest {
 	private Vendeur V2;
 	private Manager M1;
 	private Manager M2;
+	private Entreprise E;
 	ArrayList <Employe> listEmploye;
 	ArrayList <Vendeur> listVendeur;
 	ArrayList <Manager> listManager;
@@ -28,6 +31,7 @@ public class EmployeTest {
 		V2 = new Vendeur("Tou Mariam", "07 poulet", 3, 200 );
 		 M1 = new Manager("Somé Maël", "08 rue Poissy", 10); 
 		M2 = new Manager("Kam Gaël", "01 rue Igny", 5); 
+		E = new Entreprise();
 		listEmploye=new ArrayList<Employe>();
 		listEmploye.add(E1);
 		listEmploye.add(E2);
@@ -41,17 +45,28 @@ public class EmployeTest {
 	}
 	
 	@Test
-	public void SalaireTotalDeTousLesEmployers() {
-		
-		
-		for(int i=0;i < listEmploye.size();i++)
-			salaireTotal+=listEmploye.get(i).calculSalaire();
-		
-		for(int i=0;i < listVendeur.size();i++)
-			salaireTotal+=listVendeur.get(i).calculSalaireVendeur();	
-		
-		for(int i=0;i < listManager.size();i++)
-			salaireTotal+=listManager.get(i).calculSalaireManager();	
+	public void CalculSalaireDansEmployer() {
+		 double sal1= 1500+2*5;
+		 assertTrue(E1.calculSalaire() == sal1);
 	
+	}
+	
+	@Test
+	public void CalculSalaireVendeur() {
+		 double sal2= 1500+2*5+100;
+		 assertTrue(V1.calculSalaireVendeur() == sal2);
+	}
+	
+	@Test
+	public void CalculSalaireManager() {
+		 double sal3= 1500+2*10+100;
+		 assertTrue(M1.calculSalaireManager() == sal3);
+	}
+	
+	@Test
+	public void CalculSalaireTotalDeTousLesEmployers() {
+		double total = 8116;
+		 assertTrue(E.sommeTotale() == total);	
+		
 	}
 }
